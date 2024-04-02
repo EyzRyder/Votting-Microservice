f4a2ef18f9154406a36cfaa38bbf5cbca17eac16
EyzRyder <gabriel.bessi.gb@gmail.com>
2024-04-01 21:38:57 -0300

    feat: added pollOptions stuct

--- models/schema.go
+++ models/schema.go
@@ -11,3 +11,8 @@ type Poll struct {
     UpdatedAt time.Time `json:"updatedAt"`
 }
 
+type PollOption struct {
+	Id       string `json:"id"`
+	Title    string `json:"title"`
+	PollId   string `json:"pollId"`
+}
